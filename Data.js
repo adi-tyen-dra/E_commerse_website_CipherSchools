50860648d13252673710a2778df0b721602066ae
adi-tyen-dra <100347890+adi-tyen-dra@users.noreply.github.com>
2022-07-16 06:31:54 +0530

    Add files via upload

---
+++ Data.js
@@ -0,0 +1,37 @@
+export default {
+  users: [
+    { username: "regular", accessLevel: 1, password: "password" },
+    { username: "admin", accessLevel: 0, password: "password" }
+  ],
+  initProducts: [
+    {
+      
+      name: "Shoes",
+      stock: 10,
+      price: 3999,
+      shortDesc: "Comfortable, sustainable design, sturdy.",
+      img:"https://m.media-amazon.com/images/I/51TxqmHOowL.jpg"
+    },
+    {
+      name: "Hand bag",
+      stock: 20,
+      price: 8999,
+      shortDesc: "Baby pink, shiny bag for women",
+      img:"https://5.imimg.com/data5/AV/GW/MY-25291671/platinium-hand-bag-blue-500x500.jpg"
+    },
+    {
+      name: "Shirt",
+      stock: 15,
+      price: 1499,
+      shortDesc: "Navy blue, light cotten shirt with check pattern",
+      img:"https://5.imimg.com/data5/UT/MW/MY-37453636/mens-check-shirt-500x500.jpg"
+    },
+    {
+      name: "Shorts",
+      stock: 5,
+      price: 700,
+      shortDesc: "Comfy shorts with breathable fabric",
+      img:"https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1586377813-summer19-mens-trackhouse-short-forest-1586377809.jpg"
+    }
+  ]
+};
